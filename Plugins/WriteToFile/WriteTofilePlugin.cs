25ea6937f3f9b20cb06786f5bc3af8ccc3dcffd7
Tohar Bohadana <toharbbbb@gmail.com>
2022-05-03 23:02:44 +0300

    writeToFile

---
+++ Plugins/WriteToFile/WriteTofilePlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using BasePlugin.Interfaces;
+using BasePlugin.Records;
+
+
+
+namespace CountTheWords
+{
+   
+    public class WriteTofilePlugin : IPlugin
+    {
+
+        public static string _Id = "count-words";
+        public string Id => _Id;
+
+
+        public  PluginOutput  Execute (PluginInput input)
+        {
+                Console.WriteLine("הכנס נתיב קובץ");
+                string path = Console.ReadLine();
+                File.ReadAllText(path);
+                Console.Read();
+            
+        }
+    }
+}
